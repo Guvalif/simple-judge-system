81ceae0b39111f610f85754986089af87d96291a
ayaponzu2525 <ayaponzu200525@gmail.com>
2024-12-14 14:29:51 +0900

    start_fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
