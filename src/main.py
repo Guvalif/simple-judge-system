15aafab002b634107de030f1a70683f72927951e
Kazuyuki-Takase <kazuyuki_takase@dwango.co.jp>
2024-12-25 04:45:31 +0900

    Containerize the application

--- src/main.py
+++ src/main.py
@@ -64,4 +64,4 @@ async def judge(request: Request, csv_file: UploadFile = File(...)):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app, host='127.0.0.1', port=8080)
+    uvicorn.run(app, host='0.0.0.0', port=8080)
